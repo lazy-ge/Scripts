888cd0a6e1192d3b841368ec96744c759a0d6a38
chiupam <65508083+chiupam@users.noreply.github.com>
2020-07-23 22:29:15 +0800

    Update caiyun.js

--- caiyun.js
+++ caiyun.js
@@ -1,5 +1,5 @@
 /*
-此脚本基于Peng-YM的caiyun.js(https://raw.githubusercontent.com/Peng-YM/QuanX/master/Tasks/caiyun.js)改写,不需要进入box.js内进行填入彩云天气API和腾讯地图API,经纬度,使用苹果自带天气APP获取当前定位(不可在天气APP中手动添加别的城市天气)
+此脚本基于Peng-YM的caiyun.js(https://raw.githubusercontent.com/Peng-YM/QuanX/master/Tasks/caiyun.js)改写,不需要进入box.js内进行填入彩云天气API和腾讯地图API,经纬度使用苹果自带天气APP获取当前定位(不可在天气APP中手动添加别的城市天气)
 此脚本必须下载到本地文件夹中,并在该本地脚本中的第27行双引号内填入彩云天气API,第28行双引号内填入腾讯地图API
 配置:
 1.配置自动定位
@@ -25,7 +25,7 @@ type=http-request, pattern=https:\/\/((weather-data\.apple)|(api.weather))\.com,
 */
 
 var caiyun_api = "";  //双引号内彩云天气API,自行申请,申请地址:https://dashboard.caiyunapp.com/user/sign_up/
-var tencent_api = "";  //双引号内腾讯地图API,自行申请,申请地址:https://lbs.qq.com/
+var tencent_api = "EJOBZ-HCGCX-34V4P-TKXDW-7OHHV-5GF4I";  //双引号内腾讯地图API,自行申请,申请地址:https://lbs.qq.com/
 //var lot_latitude = "";  //纬度
 //var lot_longitude = "";  //经度
 if (caiyun_api ==""){
