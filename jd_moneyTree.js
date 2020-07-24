6d0a2670237ac024df4e23ac7b88c5335dda8834
chiupam_leung <65508083+chiupam@users.noreply.github.com>
2020-07-24 23:56:56 +0800

    Update jd_moneyTree.js

--- jd_moneyTree.js
+++ jd_moneyTree.js
@@ -1,4 +1,4 @@
-const $ = new Env('京东摇钱树');
+const $ = new Env('');
 const Key = '';//单引号内自行填写您抓取的京东Cookie
 //直接用NobyDa的jd cookie
 const Notice = $.getdata('jdMoneyTreeNoticeTimes') * 1 || 2;//设置运行多少次才通知。
